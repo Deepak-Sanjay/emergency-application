d1a3f378c7c387ec0ca43bf3617ed3737cc5fd2b
Deepak Sanjay <deepaksanjay920@gmail.com>
2023-08-05 22:19:17 +0530

    added first aid interface

--- app/src/main/java/com/example/medicalapplicationsystem/Homepage.java
+++ app/src/main/java/com/example/medicalapplicationsystem/Homepage.java
@@ -13,16 +13,29 @@ public class Homepage extends AppCompatActivity {
         setContentView(R.layout.activity_homepage);
 
         CardView emergency = findViewById(R.id.cardView5);
+        CardView firstaid = findViewById(R.id.cardView4);
         emergency.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openEmergency();
             }
         });
+
+        firstaid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { openfirstaid();}
+        }
+        );
     }
     public void openEmergency()
     {
         Intent intent = new Intent(this,Emergency.class);
         startActivities(new Intent[]{intent} );
+    }
+
+    public void openfirstaid()
+    {
+        Intent intent = new Intent(this,firstaid.class);
+        startActivities(new Intent[]{intent});
     }
 }
--- app/src/main/java/com/example/medicalapplicationsystem/firstaid.java
+++ app/src/main/java/com/example/medicalapplicationsystem/firstaid.java
@@ -18,23 +18,6 @@ public class firstaid extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firstaid);
 
-        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) TextView textViewGradient = findViewById(R.id.textgrad1);
-        TextView textgrad1 = null;
-        applyTextGradient(null, "#FF0000", "#0000FF");
-    }
-
-    private void applyTextGradient(TextView textgrad1, String startColor, String endColor) {
-        TextView textView = null;
-        String text = textView.getText().toString();
-        SpannableString spannableString = new SpannableString(text);
-        ForegroundColorSpan foregroundColorSpan = new ForegroundColorSpan(android.graphics.Color.parseColor(startColor));
-        spannableString.setSpan(foregroundColorSpan, 0, text.length(), Spanned.SPAN_INCLUSIVE_INCLUSIVE);
-        textView.setText(spannableString);
-
-        Shader textShader = new LinearGradient(0, 0, textView.getPaint().measureText(text), textView.getTextSize(),
-                new int[]{android.graphics.Color.parseColor(startColor), android.graphics.Color.parseColor(endColor)},
-                null, Shader.TileMode.CLAMP);
-        textView.getPaint().setShader(textShader);
     }
 
     }
